1c5e811cc8996c81ec69079a990b6fce5818efaf
Mike Berlin <mikeberlin@me.com>
2013-03-24 12:42:02 -0500

    Initial Commit

---
+++ CoreLocationPoC/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CoreLocationPoC
+//
+//  Created by Mike Berlin on 3/24/13.
+//  Copyright (c) 2013 Mike Berlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CoreLocationPoC/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CoreLocationPoC
+//
+//  Created by Mike Berlin on 3/24/13.
+//  Copyright (c) 2013 Mike Berlin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
